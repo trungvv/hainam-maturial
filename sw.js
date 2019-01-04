05654ee92b737bb11940995317bb56e162821f16
Vo Van Trung <trungvv0511@gmail.com>
2019-01-04 09:19:35 +0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-02e6c952a51129210763.js"
+    "url": "webpack-runtime-20774fae54d82de4a9d1.js"
   },
   {
     "url": "app-0b9788ba762517de1814.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a5ec3966a165e09560c8f73ee7e9b0af"
+    "revision": "74ad868bcffa3f7f55291267460824a5"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
