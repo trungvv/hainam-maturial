8feb91fc29ea52c1994fe141802a1c3e90240428
Vo Van Trung <trungvv0511@gmail.com>
2019-01-06 04:03:57 +0700

    Updates

--- sw.js
+++ sw.js
@@ -26,17 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8e3d08567c9b39654f0a.js"
+    "url": "webpack-runtime-6d516a2da1fa189c4ae0.js"
   },
   {
-    "url": "app-5ab29064b44e92d072ea.js"
+    "url": "app-4cf9ad5e14fca3f9d996.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-7c0709725b63af423850.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7df3aa2a3c7bb9f4fc42a4610b5b59c9"
+    "revision": "db7f66a232bb19abde06472e832c1361"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
